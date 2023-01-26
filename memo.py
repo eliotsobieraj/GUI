d564baea5356d3a0d5402fc35dfe82609c2028be
Eliott Sobieraj <eliott.sobierajpecole-it.com>
2023-01-26 12:20:18 +0100

    creation of breakit project

--- memo.py
+++ memo.py
@@ -1,6 +1,7 @@
 import random
+import os
 
-def affichag(tab):
+def affichage(tab):
     for i in range(len(tab)):
         print(" ".join(tab[i]))
 
@@ -12,6 +13,7 @@ def tab_secret():
         for x in range(5):
             line.append("?")
         tab.append(line)
+    return tab
 
 
 def memo_table():
@@ -31,17 +33,27 @@ def memo_main():
     tab_val = memo_table()
     count = 20
 
+
     tab_sec= tab_secret()
+
     xval, yval = 0, 0
-    xval = int(input("entrez le x de la veleur"))
-    yval = int(input("entrez le y de la valeur"))
-    values1 = [yval,xval]
-    xval = int(input("entrez le x de la veleur"))
-    yval = int(input("entrez le y de la valeur"))
-    values2 = [yval, xval]
-    print(tab_val)
-    if tab_val[int(values1[0])][int(values1[1])] == tab_val[int(values2[0])][int(values2[1])]:
-        print("gagné")
+    affichage(tab_val)
+
+    while count >0:
+        os.system("cls")
+        xval = int(input("entrez le x de la veleur"))
+        yval = int(input("entrez le y de la valeur"))
+        values1 = [yval,xval]
+        xval = int(input("entrez le x de la veleur"))
+        yval = int(input("entrez le y de la valeur"))
+        values2 = [yval, xval]
+        if tab_val[int(values1[0])][int(values1[1])] == tab_val[int(values2[0])][int(values2[1])]:
+            tab_sec[values1[0]][values1[1]] = tab_val[values1[0]][values1[1]]
+            tab_sec[values2[0]][values2[1]] = tab_val[values2[0]][values2[1]]
+            affichage(tab_sec)
+
+
+
 
 
 
